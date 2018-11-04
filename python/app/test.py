772710961c56da9a2c2e228118d8acf1705d7463
Michael santilli <michaelsantilli@Michaels-MBP-2.hsd1.pa.comcast.net>
2018-11-04 17:38:38 -0500

    Commit

---
+++ python/app/test.py
@@ -0,0 +1,12 @@
+name = raw_input("What is your name? ")
+quest = raw_input("What is your quest? ")
+color = raw_input("What is your favorite color? ")
+
+print "Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." % (name, quest, color)
+
+from datetime import datetime
+
+now = datetime.now()
+
+print now.month
